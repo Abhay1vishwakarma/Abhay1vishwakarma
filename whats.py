3bd7893839071660889ecb28fccf7bb6edbcb5db
Abhay1vishwakarma <118156018+Abhay1vishwakarma@users.noreply.github.com>
2022-12-21 20:22:45 +0530

    Add files via upload

---
+++ whats.py
@@ -0,0 +1,17 @@
+# import pywhatkit
+# i=1
+# while i<50:
+#       pywhatkit.sendwhatmsg('+91 8172939481',"hii vivek",19,45)
+      
+      
+# import pyautogui
+# import time
+# time.sleep(15)
+
+
+# i=1
+# while i<=100:
+#   pyautogui.typewrite("hii di aajkal yaad nhi aati")
+#   pyautogui.press("enter")
+#   i+=1      
+
