7bceacded2651d188cde3139f947e3ed807dff03
Abhay1vishwakarma <118156018+Abhay1vishwakarma@users.noreply.github.com>
2023-01-06 11:46:53 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,19 @@
+user=int(input("enter number: "))
+lis=[]
+a=user
+r=user
+i=0
+r=r%2
+lis.append(r)
+while i<user:
+    a=a//2
+    if(a==1):
+      lis.append(1)
+      break
+    r=a%2
+    lis.append(r)
+    i=-1
+#print(lis)
+lis.reverse()
+print("binary number is = ",lis)    
+    
---
+++ exceptionsprac.py
@@ -0,0 +1,7 @@
+
+a=5
+b="hii"
+try:
+   print(a+b)
+except:
+    print('hello world')   
